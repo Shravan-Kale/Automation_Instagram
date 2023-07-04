92d88c36d2941dca9f45cb0ed01a9d7e5fda8ab7
Shravan Kale <63283427+Shravan-kale@users.noreply.github.com>
2023-07-04 22:01:51 +0530

    Helper Scripts

---
+++ Bot/Constant.py
@@ -0,0 +1,18 @@
+import string
+
+first_names_m = ["Rahul", "Soham", "Piyush", "Vaibhav", "Vedant", "Sanket"]
+first_names_f = ["Priya", "Rashmi", "Roshni", "Ankita"]
+last_names = ["Gupta", "Sharma", "Raghuwanshi", "Kapor"]
+
+instagram_signup_url = "https://www.instagram.com/accounts/emailsignup/"
+temp_mail_url = "https://temp-mail.org/en/"
+
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+password_alphabets = letters + digits + special_chars
+username_alphabets = letters + digits
+
+# Scrape temp mail from sites and save them. Find mails that can be revisited afterward.
+# ex temp-mail.org provides qr that can be used to revisit the mail.
+mail_list = ["example.com"]
---
+++ Bot/Helper.py
@@ -0,0 +1,62 @@
+from Bot import Constant
+
+from calendar import isleap
+import secrets
+from random import randint
+
+
+def GetRandomMail():
+    return Constant.mail_list[-1]
+
+
+def GetVerificationCodeUser() -> str:
+    code = input("Enter Verification Code")
+    return code
+
+
+def GetVerificationCodeAuto() -> str:
+    return "123456"
+
+
+def GetRandomName(gender: str) -> str:
+    if gender.lower() == "m":
+        f_name = Constant.first_names_m[randint(0, len(Constant.first_names_m) - 1)]
+    else:
+        f_name = Constant.first_names_f[randint(0, len(Constant.first_names_f) - 1)]
+    l_name = Constant.last_names[randint(0, len(Constant.last_names) - 1)]
+    return f"{f_name} {l_name}"
+
+
+def GetRandomDOB() -> str:
+    day = randint(1, 31)
+    month = randint(1, 12)
+    year = randint(1950, 2000)
+    if CheckDate(day, month, year):
+        return f"{day}/{month}/{year}"
+    else:
+        GetRandomDOB()
+
+
+def GetRandomPassword() -> str:
+    password = ''
+    for i in range(10):
+        password += ''.join(secrets.choice(Constant.password_alphabets))
+    return password
+
+
+def GetRandomUsername(name: str) -> str:
+    digit = randint(0, 1000)
+    return name[0].upper() + "_" + name.split()[1] + str(digit)
+
+
+def CheckDate(day: int, month: int, year: int) -> bool:
+    if month in [4, 6, 9, 11] and day <= 30:
+        return True
+    elif month in [1, 3, 5, 7, 8, 10, 12] and day <= 31:
+        return True
+    elif isleap(year) and month == 2 and day <= 29:
+        return True
+    elif month == 2 and day <= 28:
+        return True
+    else:
+        return False
